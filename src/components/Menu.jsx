d2e0c6f747cb0f13f00f75c47bfacb6861437d1d
stwhello <sherinpaul2001@gmail.com>
2025-12-13 16:22:19 +0530

    implement the Art section

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,6 +4,10 @@ const Menu = () => {
         <section id='menu' aria-labelledby='menu-heading'>
             <img src='/images/slider-left-leaf.png' alt='left-leaf' id='m-left-leaf'/>
             <img src='/images/slider-right-leaf.png' alt='right-leaf' id='m-right-leaf'/>
+
+            <h2 id='menu-heading' className='sr-only'>
+                Cocktail Menu
+            </h2>
         </section>
     )
 }
