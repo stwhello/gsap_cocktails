21826d106969c73b7b0c9d5b8a6eccf298435f97
stwhello <sherinpaul2001@gmail.com>
2025-12-14 14:40:59 +0530

    implement the contact section

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -27,6 +27,8 @@ const Menu = () => {
         gsap.fromTo('.details p', {yPercent: 100, opacity: 0}, {
             yPercent: 0, opacity: 100, ease: 'power1.inOut',
         });
+
+
     }, [currentIndex]);
 
     const totalCocktails = sliderLists.length;
